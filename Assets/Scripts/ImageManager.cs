522137412c9ff1acbcbe16dcb6336d152f8b52cd
Tristan Jardon <tristan.jardon@gmail.com>
2017-12-11 17:05:46 +0100

    Image refresh on changing

---
+++ Assets/Scripts/ImageManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageManager : MonoBehaviour
+{
+    #region Public Members
+
+    public Transform m_leftButton;
+    public Transform m_rightButton;
+    public Image[] m_imagePack;
+    public Image m_avatar;
+
+    #endregion
+
+    #region Public void
+
+    public void GoLeft()
+    {
+        goLeft = true;
+    }
+    public void GoRight()
+    {
+        goRight= true;
+    }
+
+    #endregion
+
+    #region System
+
+    void Start()
+    {
+        m_selected = 0;
+        m_imageTansform = m_avatar.GetComponent<Transform>();
+        m_position = m_imageTansform.position;
+    }
+	
+	void Update()
+    {
+        if (goLeft)
+        {
+            m_imageTansform.position = m_imageTansform.position + Vector3.left * .5f ;
+            if(m_imageTansform.position.x < -10)
+            {
+                goLeft = false;
+                m_selected = m_random.Next(0, 5);
+                m_imageTansform.position = m_position;
+                m_avatar.sprite = m_imagePack[m_selected].sprite;
+
+            }
+        }
+        if (goRight)
+        {
+            m_imageTansform.position = m_imageTansform.position + Vector3.right * .5f;
+            if (m_imageTansform.position.x > 10)
+            {
+                goRight = false;
+            }
+        }
+    }
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+    #region Private an Protected Members
+
+    private bool goLeft = false;
+    private bool goRight = false;
+    private int m_selected;
+    private Transform m_imageTansform;
+    private Vector3 m_position = new Vector3();
+    private System.Random m_random = new System.Random();
+
+    #endregion
+}
