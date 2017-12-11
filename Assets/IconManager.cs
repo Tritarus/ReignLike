11139998d83781f64e7cad2d69b7c6ba61bcf671
Tristan Jardon <tristan.jardon@gmail.com>
2017-12-11 12:04:31 +0100

    Interface v1

---
+++ Assets/IconManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconManager : MonoBehaviour
+{
+    #region Public Members
+
+    #endregion
+
+    #region Public void
+
+    #endregion
+
+    #region System
+
+    void Awake()
+    {
+        m_transform = gameObject.GetComponent<Transform>();
+
+        foreach(Transform child in m_transform)
+        {
+            
+        }
+	}
+	
+	void Update()
+    {
+		
+	}
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+    #region Private an Protected Members
+
+    private Transform m_transform;
+    private Transform[] m_indicators;
+
+    #endregion
+}
