88f4103bfb803112df686acc527e220f966b9186
Tristan Jardon <tristan.jardon@gmail.com>
2017-12-12 16:07:54 +0100

    add stuff

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : ReignsUI
+{
+
+    #region Public Members
+
+    #endregion
+
+    #region Public void
+
+    public override void HighlighStatusPossiblity(int index, float pourcentageStatus, float nextPourcentStatus)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void HighlighStatusPossiblity(string objectiveName, float pourcentageStatus, float nextPourcentStatus)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetAvatarImage(Sprite texture)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetAvatarName(string dialogue)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetBonusIcon(int index, Sprite iconTexture)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetBonusIcon(string questId, Sprite iconTexture)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetBonusStatus(int index, bool isComplete)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetBonusStatus(string questId, bool isComplete)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetContextBackground(string idToLoad)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetContextBackground(Sprite texture)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetDialogue(string dialogue)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void SetImageName(string imageIdName)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetKingName(string kingName)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetLeftAnswer(string text)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetObjectiveStatus(int index, float pourcentageStatus)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetObjectiveStatus(string objectiveId, float pourcentageStatus)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetRightAnswer(string text)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetScoreTo(int score)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetYearOfReigns(int years)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    #endregion
+
+    #region System
+
+    void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		
+	}
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+    #region Private an Protected Members
+
+    #endregion
+}
